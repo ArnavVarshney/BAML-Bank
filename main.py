a86c7062f58be80e9369d47066e14de7286de241
ArnavVarshney <varshney.arnav@gmail.com>
2019-11-27 18:40:47 +0800

    Changed menu items

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 def intro():
-    print('Welcome to the Bank')
-    print('\t\tMAIN MENU')
+    ch = ''
+    while True:
+        print('\t  Welcome to XXX')
+        print('\t\tMAIN MENU')
+        print('#. About')
+        print('*. Exit')
+        ch = input('Enter your choice: ')
+        if ch == '#':
+            aboutUs()
+        elif ch == '*':
+            print("Goodbye!")
+            break
 
-if(__name__ == "__main__"):
+
+def aboutUs():
+    print('Team XXX')
+    print('Members:')
+    print('\tArnav\n\tPradyumn\n\tAditi\n\tMihir\n\tShishir\n')
+    intro()
+
+
+if __name__ == "__main__":
     intro()
