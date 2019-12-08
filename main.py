9676720cf5d4307fd7e4d2161f7f1caffd137ffb
Arnav Varshney <varshney.arnav@gmail.com>
2019-12-08 14:21:51 +0530

    main.py is over *PERIOD*
    Signed-off-by: Arnav Varshney <varshney.arnav@gmail.com>

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ global_transactions = []  # global transaction log
 
 
 class Branch(object):
+    """
+    Maintains a structure for all branches
+    :param str branch_code: branch code of the branch
+    :param str branch_name: name of branch
+    :param Address address: address of the branch
+    """
+
     def __init__(self, branch_code, branch_name, address):
         self.branch_code = branch_code
         self.branch_name = branch_name
         self.address = address
 
     def __str__(self):
+        """
+        :return printable string for an object of Branch class
+        :rtype str
+        """
         return 'Branch Name: ' + self.branch_name + '\n' + 'Branch Code: ' + self.branch_code + '\n' + str(self.address)
 
 
